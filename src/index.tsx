2a296045c7e1ee3c7793eec7cb838182c461cce8
Kyle Flynn <kyle.flynn5798@yahoo.com>
2019-08-02 16:20:17 -0500

    Initial commit.

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,8 @@
-import * as React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
-// console.log((window as any).__AXIOS_HOOKS_CACHE__);
-
-// @ts-ignore - Axios Hooks .d.ts doesn't export this for some reason.
-const development: boolean = true;
-
-if (!development) {
-  const {loadCache} = require("axios-hooks");
-  loadCache((window as any).__AXIOS_HOOKS_CACHE__);
-  delete (window as any).__AXIOS_HOOKS_CACHE__;
-}
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
