16026671bd72a26c218ff53840abd24389b1bcad
Kyle Flynn <kyle.flynn5798@yahoo.com>
2019-08-13 13:33:26 -0500

    Fixed bug with client not rednering due to undefined stateCache.

--- client.tsx
+++ client.tsx
@@ -6,22 +6,27 @@ import * as AxiosHooks from "axios-hooks";
 
 import {Provider} from "react-redux";
 import {createStore} from "redux";
-import appReducer from "./src/store/Reducer";
+import appReducer, {initialState} from "./src/store/Reducer";
 
 import {FGCProvider} from "@the-orange-alliance/lib-ems";
 
+// Dealing with our application caches -- Axios and Redux
 (AxiosHooks as any).loadCache((window as any).__AXIOS_HOOKS_CACHE__);
 delete (window as any).__AXIOS_HOOKS_CACHE__;
-
 const stateCache = (window as any).__REDUX_STATE_CACHE__;
+const isDev: boolean = true;
 
-const isDev: boolean = false;
 
 FGCProvider.initialize("127.0.0.1", 8088);
 
+let state = stateCache;
+if (stateCache.length > 0 && stateCache[0] === "__REDUX__") {
+  state = initialState;
+}
+
 if (isDev) {
   ReactDOM.render(
-    <Provider store={createStore(appReducer, stateCache)}>
+    <Provider store={createStore(appReducer, state)}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
@@ -30,7 +35,7 @@ if (isDev) {
   );
 } else {
   ReactDOM.hydrate(
-    <Provider store={createStore(appReducer, stateCache)}>
+    <Provider store={createStore(appReducer, state)}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
