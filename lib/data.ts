343e90e9a474575577c801530d56c346e3e5b3da
Ofek Ashery <ofek.ashery@gmail.com>
2022-10-16 10:02:42 +0300

    Update livestreams for day3

--- lib/data.ts
+++ lib/data.ts
@@ -1,9 +1,9 @@
 export const watchLinks = {
   all: "https://www.youtube.com/playlist?list=PL-RL-gR4GAffgbkM4fIChO3EpT2hrixXl",
-  main: "https://youtu.be/eyX_xsrsSeY",
-  field1: "https://youtu.be/K3cFc0bU00M",
-  field2: "https://youtu.be/ntW0GTPqS4o",
-  field3: "https://youtu.be/zEJfTRKKoC4",
-  field4: "https://youtu.be/yl7pcSsFbmo",
-  field5: "https://youtu.be/qm2vjMfBa5Q",
+  main: "https://youtu.be/EcLRHMtLKmo",
+  field1: "https://youtu.be/1Ev9UTCE2AU",
+  field2: "https://youtu.be/dDvs0-hzevM",
+  field3: "https://youtu.be/XJKNKyfe7-4",
+  field4: "https://youtu.be/azbs8YD9xjc",
+  field5: "https://youtu.be/zZc9wlJSdpc",
 };
