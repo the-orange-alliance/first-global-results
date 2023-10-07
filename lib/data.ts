e3a3c5911d67bd715562476ed4a33c622cda9b06
Ofek Ashery <ofek.ashery@gmail.com>
2023-10-07 11:27:01 +0300

    Add Opening Ceremony live

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,6 @@
 export const watchLinks = {
   all: "https://www.youtube.com/playlist?list=PL-RL-gR4GAffgbkM4fIChO3EpT2hrixXl",
-  main: "https://youtu.be/EcLRHMtLKmo",
+  main: "https://youtu.be/p_NnTnd3rEI",
   field1: "https://youtu.be/1Ev9UTCE2AU",
   field2: "https://youtu.be/dDvs0-hzevM",
   field3: "https://youtu.be/XJKNKyfe7-4",
