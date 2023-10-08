5ca41c50c221dec135685874fd966055e27a4d11
Soren Zaiser <soren.zaiser@gmail.com>
2023-10-07 20:16:10 -0400

    Update stream links

--- lib/data.ts
+++ lib/data.ts
@@ -1,9 +1,9 @@
 export const watchLinks = {
-  all: "https://www.youtube.com/playlist?list=PL-RL-gR4GAffgbkM4fIChO3EpT2hrixXl",
-  main: "https://youtu.be/EcLRHMtLKmo",
-  field1: "https://youtu.be/1Ev9UTCE2AU",
-  field2: "https://youtu.be/dDvs0-hzevM",
-  field3: "https://youtu.be/XJKNKyfe7-4",
-  field4: "https://youtu.be/azbs8YD9xjc",
-  field5: "https://youtu.be/zZc9wlJSdpc",
+  all: "https://first.global/live",
+  main: "https://first.global/live",
+  field1: "https://youtu.be/Lyz2dyuYOVA",
+  field2: "https://youtu.be/5X5IVaAKOc4",
+  field3: "https://youtu.be/FesF4umkqKg",
+  field4: "https://youtu.be/43vi8mebumc",
+  field5: "https://first.global/live",
 };
