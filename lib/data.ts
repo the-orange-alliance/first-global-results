04f9ebd352f011dd1bf9105a8388aad57a500418
Soren Zaiser <soren.zaiser@gmail.com>
2023-10-08 21:21:40 -0400

    Day 2 streams

--- lib/data.ts
+++ lib/data.ts
@@ -1,9 +1,9 @@
 export const watchLinks = {
   all: "https://first.global/live",
   main: "https://first.global/live",
-  field1: "https://youtu.be/Lyz2dyuYOVA",
-  field2: "https://youtu.be/5X5IVaAKOc4",
-  field3: "https://youtu.be/FesF4umkqKg",
-  field4: "https://youtu.be/43vi8mebumc",
+  field1: "https://youtu.be/IgS8z1B1yg0",
+  field2: "https://youtu.be/zGAcQWph9qw",
+  field3: "https://youtu.be/EDLcrV-ZWCs",
+  field4: "https://youtu.be/9Cgij9hvTjQ",
   field5: "https://first.global/live",
 };
