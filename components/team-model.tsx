536cd06dbb7e3123389ea807b28e3355fec66d9d
Soren Zaiser <soren.zaiser@gmail.com>
2023-10-08 21:52:47 -0400

    Update title #2

--- components/team-model.tsx
+++ components/team-model.tsx
@@ -141,7 +141,7 @@ const TeamModel: React.FC<TeamModelProps> = ({ country, data, onClose }) => {
           <DetailsList.Item icon={<HighestScoreIcon />} title="Highest Score">
             {rank.highestScore}
           </DetailsList.Item>
-          <DetailsList.Item icon={<HydrogenIcon />} title="Energy Points">
+          <DetailsList.Item icon={<HydrogenIcon />} title="Total Oxygen + Hydrogen Points">
             {rank.oxyHydroPoints}
           </DetailsList.Item>
           <DetailsList.Item icon={<MatchesPlayedIcon />} title="Matches Played">
