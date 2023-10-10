6dc6cddc60f39079549d3aaf79fa6f86e5b38f27
Soren Zaiser <soren.zaiser@gmail.com>
2023-10-09 22:49:26 -0400

    Update links day 3

--- lib/data.ts
+++ lib/data.ts
@@ -1,9 +1,9 @@
 export const watchLinks = {
   all: "https://first.global/live",
   main: "https://first.global/live",
-  field1: "https://youtu.be/IgS8z1B1yg0",
-  field2: "https://youtu.be/zGAcQWph9qw",
-  field3: "https://youtu.be/EDLcrV-ZWCs",
-  field4: "https://youtu.be/9Cgij9hvTjQ",
+  field1: "https://www.youtube.com/watch?v=hQeEWxXgboc",
+  field2: "https://www.youtube.com/watch?v=96H0_qOIa84",
+  field3: "https://www.youtube.com/watch?v=jT_fh7fiG0c",
+  field4: "https://www.youtube.com/watch?v=1W5KtABbbGw",
   field5: "https://first.global/live",
 };
